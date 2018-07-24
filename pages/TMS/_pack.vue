9a53966a4bbbb4aed26c0f2dab02e3af728aa575
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-07-24 19:35:08 -0400

    Setup logic for moving to "next step"

--- pages/TMS/_pack.vue
+++ pages/TMS/_pack.vue
@@ -15,13 +15,27 @@
 			<v-layout justify-center>
 				<v-flex xs11 sm10 md9 lg8>
 					<v-expansion-panel popout>
-						<v-expansion-panel-content ripple v-for="(topic, i) in pack.topics" :key="i">
+						<v-expansion-panel-content :key="i" ripple v-for="(topic, i) in pack.topics">
 							<div class="title" slot="header">{{ topic.title }}</div>
 							<v-container grid-list-lg>
 								<v-layout justify-center row wrap>
 									<v-flex :key="i" v-for="(verse, i) in topic.verses" xs12 md8 lg6>
-										<v-card class="grey lighten-3">
-											<v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+										<v-card class="grey lighten-5" height="225px">
+											<v-card-text v-if="stage[verse.number] < 1 || !stage[verse.number]">
+												<v-layout align-center justify-center>
+													<p class="title"><strong>{{ verse.number.toUpperCase() }}</strong></p>
+												</v-layout>
+											</v-card-text>
+											<v-card-text v-if="stage[verse.reference] == 1">
+												<div>
+													<p><strong>{{ verse.reference }}</strong></p>
+													<p>{{ verse.body['esv'] }}</p>
+												</div>
+											</v-card-text>
+											<v-card-actions>
+												<v-spacer></v-spacer>
+												<v-btn color="deep-orange" @click="next(verse.reference)" dark>Explore</v-btn>
+											</v-card-actions>
 										</v-card>
 									</v-flex>
 								</v-layout>
@@ -41,18 +55,26 @@ import info from '~/static/info/tms.json'
 export default {
 	asyncData ({ app, params, error }) {
 		let data = info[params.pack.toLowerCase()]
+		let stage = {}
 		if (!data) {
 			error({ statusCode: 404, message: 'Pack not found' })
 		}
+		for (let i in data.topics) {
+			for (let verse in data.topics[i].verses) {
+				stage[data.topics[i].verses[verse].reference] = 0
+			}
+		}
+		console.log(stage)
 		return {
 			pack: data,
-			packName: params.pack
+			packName: params.pack,
+			stage: stage
 		}
 	},
-	data () {
-		return {
-			note: null,
+	methods: {
+		next (number) {
+			this.stage[number] += 1
 		}
-	},
+	}
 }
 </script>
